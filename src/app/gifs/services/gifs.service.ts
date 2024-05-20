4fc034c8e7965e8a634bb84fb59b45be6b17efd2
EmanuelLicona <emanuel.licona@gmail.com>
2024-05-19 22:14:59 -0600

    agregando servicio http y vista de gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,13 +1,24 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interface';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
-
+  private _gifList: Gif[] = []
   private _tagsHistory: string[] = []
-  constructor() { }
+  private apiKey = 'WcJFniX6j1UyPsxsqFyhDFJQoPFKt65m'
+  private urlBase = `https://api.giphy.com/v1/gifs`
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
   get tagsHistory(): string[] {
     return [...this._tagsHistory];
+  }
+
+  get gifList(): Gif[] {
+    return this._gifList;
   }
 
   private organizeHistory(tag: string) {
@@ -20,9 +31,29 @@ export class GifsService {
 
   searchTag(tag: string): void {
     if (tag.length == 0) return;
+    if(this.tagsHistory.indexOf(tag) === 0) return
 
     this.organizeHistory(tag)
-  }
 
+    // fetch(this.urlBase + tag)
+    //   .then(resp => resp.json())
+    //   .then(resp => {
+    //     this.organizeHistory(tag)
+    //     console.log(resp)
+    //   })
+    //   .catch(error => {
+    //     console.error(error)
+    //   })
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', 10)
+      .set('q', tag);
+
+    this.http.get<SearchResponse>(this.urlBase + '/search', { params })
+      .subscribe(resp => {
+        this._gifList = resp.data;
+      });
+  }
 
 }
